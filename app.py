93405ad43eadc06a3b4f685f2e4672b1c5da7e7c
adamfinklecloudberry <198999961+adamfinklecloudberry@users.noreply.github.com>
2025-02-13 07:35:26 -0500

    Added feedback

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 from flask import Flask, render_template, request
 
+
 app = Flask(__name__)
+
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    """Prompts the user to enter a name and displays it"""
     greeting = ""
     if request.method == "POST":
         username = request.form["name"]
         greeting = f"Hello, {username}!"
     return render_template("index.html", greeting=greeting)
+
+
+@app.route("/feedback", methods=["GET", "POST"])
+def feedback():
+    """Prompts the user to enter feedback and displays it"""
+    response = ""
+    if request.method == "POST":
+        feedback = request.form["feedback"]
+        response = f"Thank you for your feedback: '{feedback}'"
+    return render_template("feedback.html", response=response)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
